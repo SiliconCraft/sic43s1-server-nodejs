3698d2ce6d464cd133bc3db666d1f325f37ba396
pichaya <pichaya@sic.co.th>
2020-06-25 16:57:40 +0700

    - add web.config

--- index.js
+++ index.js
@@ -57,11 +57,11 @@ router.get('/', function(req, res) {
 app.use("/", router);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function (req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 app.set('port', process.env.PORT || 3000);
 
